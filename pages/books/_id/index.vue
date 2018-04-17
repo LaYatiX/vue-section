999781c06c983e3fa46d4f295c9b09df531d1644
darqwski <dariusz.cabala.pl@gmail.com>
2018-04-17 22:10:18 +0200

    Styling books, and Book-Container

--- pages/books/_id/index.vue
+++ pages/books/_id/index.vue
@@ -1,22 +1,52 @@
 <template>
   <section class="single-book">
-      <h1>{{book.title}}</h1>
-    <div>
-        <img :src="book.img" class="book-img" :alt="book.title">
+    <div class="book-title">
+      <h1 v-show="shown.title" @dblclick="edit('title')">{{book.title}}</h1>
+      <input v-show="!shown.title" type="text" v-model="book.title" @blur="reset" @keyup.enter="reset">
     </div>
-    <p class="book-title">Autor: {{book.author}}</p>
-    <p class="book-description">Opis: {{book.description}}</p>
+    <div>
+      <img :src="book.img" class="book-img" :alt="book.title">
+    </div>
+    <div class="book-author">
+      <p v-show="shown.author" @dblclick="edit('author')">Autor: {{book.author}}</p>
+      <input v-show="!shown.author" type="text" v-model="book.author" @blur="reset" @keyup.enter="reset">
+    </div>
+    <div class="book-description">
+      <p v-show="shown.description" @dblclick="edit('description')">Opis: {{book.description}}</p>
+      <input v-show="!shown.description" type="text" v-model="book.description" @blur="reset" @keyup.enter="reset">
+    </div>
   </section>
 </template>
 
 <script>
-export default {
-  computed: {
-    book () {
-      return this.$store.state.books[this.$route.params.id - 1]
+  export default {
+    data () {
+      return {
+        shown: {},
+        defaultShown: {
+          title: true,
+          author: true,
+          description: true
+        }
+      }
+    },
+    created () {
+      this.reset()
+    },
+    computed: {
+      book () {
+        return this.$store.state.books[this.$route.params.id - 1]
+      }
+    },
+    methods: {
+      edit (property) {
+        this.shown[property] = false
+      },
+      reset () {
+        this.shown = JSON.parse(JSON.stringify(this.defaultShown))
+      }
     }
   }
-}
 </script>
 <style scoped>
   .single-book {
